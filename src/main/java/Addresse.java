6ee512da8656bbbf75b3dc1b05f2d577217a0a0f
dmitriisu333 <dmitrii.su333@gmail.com>
2022-06-26 16:58:22 +0200

    Klasse Addresse und Student bearbeitet

--- src/main/java/Addresse.java
+++ src/main/java/Addresse.java
@@ -1,2 +1,56 @@
 public class Addresse {
+
+    private int hausnummer;
+    private String strasse;
+    private String stadt;
+    private String zip;
+    private String kanton;
+
+    public Addresse(int hausnummer, String strasse, String stadt, String zip, String kanton) {
+        this.hausnummer = hausnummer;
+        this.strasse = strasse;
+        this.stadt = stadt;
+        this.zip = zip;
+        this.kanton = kanton;
+    }
+
+    public int getHausnummer() {
+        return hausnummer;
+    }
+
+    public void setHausnummer(int hausnummer) {
+        this.hausnummer = hausnummer;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getStadt() {
+        return stadt;
+    }
+
+    public void setStadt(String stadt) {
+        this.stadt = stadt;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getKanton() {
+        return kanton;
+    }
+
+    public void setKanton(String kanton) {
+        this.kanton = kanton;
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,6 @@
 public class Student extends Person{
+    public Student(Long id, String name, String vorname, int alter, String geschlecht, String email, String telefonnummer, Addresse adresse) {
+        super(id, name, vorname, alter, geschlecht, email, telefonnummer, adresse);
+    }
+
 }
